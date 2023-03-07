9378421c43924a5233c9199c40fbcf7c89db70cd
Meier Lukas <meierschlumpf@gmail.com>
2023-03-07 18:42:20 +0100

    initial commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,40 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+
+import { api } from "~/utils/api";
+
+import "~/styles/globals.css";
+
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+      <SessionProvider session={session}>
+        <MantineProvider
+          withGlobalStyles
+          withNormalizeCSS
+          theme={{
+            /** Put your mantine theme override here */
+            colorScheme: "light",
+          }}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </SessionProvider>
+    </>
+  );
+};
+
+export default api.withTRPC(MyApp);
